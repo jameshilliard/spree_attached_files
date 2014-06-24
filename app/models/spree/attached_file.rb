d74f077e18ad8864f4a621fb9cf2d9d4e4aa52e8
James Hilliard <james.hilliard1@gmail.com>
2014-06-24 09:21:00 -0600

    use attr_accessor

--- app/models/spree/attached_file.rb
+++ app/models/spree/attached_file.rb
@@ -1,6 +1,6 @@
 class Spree::AttachedFile < ActiveRecord::Base
   default_scope order('created_at DESC')
-  attr_accessible :attachment, :name
+  attr_accessor :attachment, :name
   validates :name, presence: true, uniqueness: true
 
   has_attached_file :attachment,
